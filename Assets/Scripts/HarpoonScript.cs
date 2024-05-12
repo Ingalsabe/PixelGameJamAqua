3f1f8cb3ccd61ef7b26e876cf7e9acc3a01c4267
Ingalsabe <103951858+Ingalsabe@users.noreply.github.com>
2024-05-12 15:49:21 -0500

    Enemies get killed by bullet/harpoon thing

--- Assets/Scripts/HarpoonScript.cs
+++ Assets/Scripts/HarpoonScript.cs
@@ -37,7 +37,7 @@ public class HarpoonScript : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.name);
         if (collision.gameObject.tag == "Enemy")
