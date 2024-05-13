920bae58ed02f1f27187c692d686056abfd86494
Ingalsabe <103951858+Ingalsabe@users.noreply.github.com>
2024-05-13 12:55:49 -0500

    ProgressBarReload added

--- Assets/Scripts/WeaponAimScript.cs
+++ Assets/Scripts/WeaponAimScript.cs
@@ -8,8 +8,10 @@ public class WeaponAimScript : MonoBehaviour
     private Vector3 mousePosition;
     private float timer;
 
+    [SerializeField] private GameObject progBar;
+
     public bool canFire;
-    public float reloadTime = 2f;
+    public float reloadTime = 1.5f;
     public float harpoonSpeed = 4.5f;
     public GameObject Harpoon;
     public Transform bulletTransform;
@@ -18,6 +20,7 @@ public class WeaponAimScript : MonoBehaviour
     void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        progBar.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,21 +30,30 @@ public class WeaponAimScript : MonoBehaviour
         Vector3 rotation = mousePosition - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
+        Vector3 currentScale = progBar.transform.localScale;
+
         transform.rotation = Quaternion.Euler(0,0,rotZ);
 
         if(!canFire)
         {
             timer += Time.deltaTime;
-            if(timer > reloadTime)
+
+            currentScale.x = timer * 0.1f;
+            progBar.transform.localScale = currentScale;
+
+            if (timer > reloadTime)
             {
                 canFire = true;
                 timer = 0;
+                progBar.SetActive(false);
+                currentScale.x = 0f;
             }
         }
 
         if(Input.GetMouseButton(0) && canFire)
         {
             canFire = false;
+            progBar.SetActive(true);
             Instantiate(Harpoon,bulletTransform.position, Quaternion.Euler(0,0,rotZ + 46));
         }
     }
@@ -51,5 +63,11 @@ public class WeaponAimScript : MonoBehaviour
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
+    }
+
+    private void ReloadProgressBarFillIn()
+    {
+        Vector3 currentScale = progBar.transform.localScale;
+
     }
 }
